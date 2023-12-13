0c35aeea2995d4b7285b1278d353987cd5e8cc2f
turtlebeasts <179turtleshells@gmail.com>
2023-12-13 14:44:09 +0530

    Restored

---
+++ src/pages/newsignin/newsignin.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './index.css';
+
+export default function NewSignIn() {
+  return (
+    <div className='main-container'>
+      <div className='rectangle' />
+      <div className='rectangle-1'>
+        <span className='natural-cancer-care-center'>
+          Natural Cancer Care Center
+        </span>
+        <div className='rectangle-2'>
+          <span className='login-3'>Login</span>
+          <span className='email'>Email</span>
+          <div className='auto-layer-row-eb'>
+            <div className='frame' />
+            <span className='email-4'>Email</span>
+          </div>
+          <span className='password'>Password</span>
+          <div className='auto-layer-row-beca'>
+            <div className='frame-5' />
+            <span className='password-6'>Password</span>
+          </div>
+          <div className='auto-layer-row-bd'>
+            <div className='frame-7'>
+              <div className='vector' />
+            </div>
+            <span className='admin'>Admin</span>
+          </div>
+          <span className='login-8'>Login</span>
+        </div>
+      </div>
+      <div className='natural-cancer-care-center-9' />
+      <div className='extensive-care'>
+        <span className='we-provide'>
+          We provide
+          <br />
+        </span>
+        <span className='ailments'>extensive </span>
+        <span className='we-provide-a'>
+          care for
+          <br />
+          all your
+        </span>
+        <span className='ailments-b'> ailments</span>
+      </div>
+      <div className='Frame' />
+    </div>
+  );
+}

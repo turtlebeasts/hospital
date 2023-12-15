bd9030c59bbb72ce5e3d81b695537aba4b6439f8
turtlebeasts <179turtleshells@gmail.com>
2023-12-15 10:09:47 +0530

    padding adjustments

--- src/containers/banner/banner.jsx
+++ src/containers/banner/banner.jsx
@@ -6,10 +6,10 @@ export default function Banner(){
         <div className="banner-image">
             <div className="banner-cover">
                 <Grid container maxWidth={"md"}>
-                    <Grid item xs={12} md={6} sx={{textAlign: {xs: 'center', sm: 'left'}}}>
-                        <Typography variant="h3" sx={{fontWeight: 900}}>We provide</Typography>
-                        <Typography variant="h3" sx={{fontWeight: 900}}><span>extensive</span> care for</Typography>
-                        <Typography variant="h3" sx={{fontWeight: 900}}>all your <span>ailments</span></Typography>
+                    <Grid item xs={12} md={6} sx={{textAlign: {xs: 'center', sm: 'left'}, display: {xs: 'flex',sm: 'block'}, flexDirection: 'column', justifyContent: {xs:'center', sm: 'left'}, alignItems: 'center'}}>
+                        <Typography variant="h3" sx={{fontWeight: 900, width: '96%'}}>We provide</Typography>
+                        <Typography variant="h3" sx={{fontWeight: 900, width: '96%'}}><span>extensive</span> care</Typography>
+                        <Typography variant="h3" sx={{fontWeight: 900, width: '96%'}}>for all your <span>ailments</span></Typography>
                         <Button variant="contained" color="primary">CONTACT US</Button>
                     </Grid>
                     <Grid item xs={12} md={6}></Grid>
--- src/containers/everready/everready.jsx
+++ src/containers/everready/everready.jsx
@@ -9,7 +9,7 @@ export default function EverReady(){
                 <Typography variant="h3">Ever ready to serve you</Typography>
                 <Typography variant="h5">Feel free to contact us</Typography>
                 <Typography variant="h6" sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}><CallIcon fontSize="large"/> 9678165454, 9395344890</Typography>
-                <Typography variant="h6" sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}><EmailIcon fontSize="large"/> naturalcancercarecentre@gmail.com</Typography>
+                <Typography variant="h6" sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: 18}}><EmailIcon fontSize="large"/> naturalcancercarecentre@gmail.com</Typography>
             </Grid>
         </Grid>
     )
